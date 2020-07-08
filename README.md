fc3023b6def8ffda6cc6356177419fdfe4aaf6b5
Hex <hex@codeigniter.org.cn>
2020-07-08 19:38:52 +0800

    重构部分代码

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Resource Uploader
 
 [![Build Status](https://circleci.com/gh/hex-ci/resource-uploader/tree/master.svg?style=shield)](https://circleci.com/gh/hex-ci/resource-uploader/tree/master) [![Windows Build status](https://ci.appveyor.com/api/projects/status/rtsoxi1ek6atyxfb?svg=true)](https://ci.appveyor.com/project/hex-ci/resource-uploader) [![npm version](https://badgen.net/npm/v/resource-uploader)](https://www.npmjs.com/package/resource-uploader) [![Downloads](https://badgen.net/npm/dt/resource-uploader)](https://www.npmjs.com/package/resource-uploader)
 
-一站式前端资源 CDN 上传工具（使用阿里云 OSS）
+一站式资源上传和处理工具（使用阿里云 OSS）
 
 ## 安装
 
@@ -13,7 +13,7 @@ npm install -g resource-uploader
 
 ## 特性
 
-* 支持 ES6/7/8 转译 ES5
+* 支持 Babel 转译
 * 支持 Javascript 压缩和优化
 * 支持 Javascript 深度混淆
 * 支持 Sass/Less 转 CSS
@@ -26,35 +26,149 @@ npm install -g resource-uploader
 * 自动设置 HTTP 缓存头，永久缓存资源在浏览器
 * 支持多配置
 * 支持处理成 BASE64 资源
-* 支持文件合并
+* 支持多文件合并
 
 ## 使用
 
-用法:
+### 上传资源到 OSS
+
+#### 用法
 
 ```
 res-up [选项] 文件
 ```
 
-选项:
+#### 选项
 
-| 选项            | 描述                                                           | 类型                   |
-|-----------------|----------------------------------------------------------------|------------------------|
-| -h, --help      | 显示帮助信息                                                   | [布尔]                 |
-| --compress, -c  | 是否压缩文件                                                   | [布尔] [默认值: true]  |
-| --obfuscate, -o | 是否开启 JS 深度混淆                                           | [布尔] [默认值: false] |
-| --prefix, -p    | 自定义 URL 路径                                                | [字符串]               |
-| --name          | 自定义 URL 文件名                                              | [字符串]               |
-| --concat        | 是否合并文件                                                   | [布尔] [默认值: false] |
-| --base64        | 是否处理成 base64 内容，而不上传 CDN                           | [布尔] [默认值: false] |
-| --dest          | 本机文件系统路径，使用此参数将保存文件到指定路径，而不上传 CDN | [字符串]               |
-| --refresh, -r   | 否刷新 CDN 资源                                                | [布尔] [默认值: false] |
-| --output-simple | 是否简化控制台输出                                             | [布尔] [默认值: false] |
-| --config        | 自定义配置文件，使用此参数可以手动指定使用哪个配置文件         | [字符串] [默认值: ""]  |
-| --init-config   | 初始化配置文件                                                 | [布尔] [默认值: false] |
-| --version, -v   | 显示版本信息                                                   | [布尔]                 |
+##### --compress
 
-示例：
+是否压缩文件
+
+简写: -c
+
+类型: boolean
+
+默认值: true
+
+#### --babel
+
+是否使用 Babel 转译 JS 文件
+
+类型: boolean
+
+默认值: true
+
+#### --iife
+
+JS 文件是否使用 IIFE（立即执行函数）包裹，启用压缩选项才可使用
+
+类型: boolean
+
+默认值: true
+
+#### --obfuscate
+
+是否开启 JS 深度混淆
+
+简写: -o
+
+类型: boolean
+
+默认值: false
+
+#### --sass
+
+是否使用 Sass 预处理器
+
+类型: boolean
+
+默认值: true
+
+#### --less
+
+是否使用 Less 预处理器
+
+类型: boolean
+
+默认值: true
+
+#### --raw
+
+是否上传原始文件
+
+类型: boolean
+
+默认值: false
+
+#### --prefix
+
+自定义 URL 路径
+
+简写: -p
+
+类型: string
+
+#### --name
+
+自定义 URL 文件名
+
+类型: string
+
+#### --base64
+
+是否处理成 base64 内容，而不上传 OSS
+
+类型: boolean
+
+默认值: false
+
+#### --dest
+
+本机文件系统路径，使用此参数将保存文件到指定路径，而不上传 OSS
+
+类型: string
+
+#### --concat
+
+是否合并文件
+
+类型: boolean
+
+默认值: false
+
+### 刷新 OSS 资源
+
+#### 用法
+
+```
+res-up refresh URL
+```
+
+### 公共选项
+
+#### --output-simple
+
+是否简化控制台输出
+
+类型: boolean
+
+默认值: false
+
+#### --config
+
+自定义配置文件
+
+类型: striing
+
+#### --init-config
+
+初始化配置文件
+
+类型: boolean
+
+默认值: false
+
+## 示例
 
 ```
 res-up filename.png
@@ -68,5 +182,5 @@ res-up --base64 --dest /Users/xxx/Desktop filename.png
 res-up --dest /Users/xxx/Desktop filename.png
 res-up --no-compress filename.png
 res-up --config ./custom-config.json filename.png
-res-up --refresh "https://domain.com/-/xxx/filename.png"
+res-up refresh "https://domain.com/-/xxx/filename.png"
 ```
--- index.js
+++ index.js
@@ -16,14 +16,16 @@ const through2 = require('through2-concurrent');
 const use = require('postcss-use');
 const readline = require('readline');
 
-const oss = require('./lib/alioss.js');
-const html = require('./lib/html.js');
-const inlineCompress = require('./lib/inline-compress.js');
-const imagemin = require('./lib/imagemin.js');
-const htmlmin = require('./lib/gulp-htmlmin.js');
-const less = require('./lib/gulp-less.js');
-const sass = require('./lib/gulp-sass.js');
-const javascriptObfuscator = require('./lib/gulp-javascript-obfuscator.js');
+const oss = require('./lib/alioss');
+const html = require('./lib/html');
+const inlineCompress = require('./lib/inline-compress');
+const imagemin = require('./lib/imagemin');
+const htmlmin = require('./lib/gulp-htmlmin');
+const less = require('./lib/gulp-less');
+const sass = require('./lib/gulp-sass');
+const javascriptObfuscator = require('./lib/gulp-javascript-obfuscator');
+const terser = require('./lib/gulp-terser');
+const wrapper = require('./lib/gulp-wrapper');
 
 const pkg = require('./package.json');
 
@@ -43,7 +45,8 @@ const babelPlugins = pkg.babelPlugins.map(item => {
 
 const $ = gulpLoadPlugins();
 
-const argv = yargs.usage('用法: $0 [选项] 文件') // usage string of application.
+const argv = yargs.usage('用法: $0 [命令] [选项] 文件')
+  .command('refresh <URL>', '刷新已存在的 OSS 资源缓存')
   .option('h', {
     alias: 'help',
     description: '显示帮助信息'
@@ -52,11 +55,31 @@ const argv = yargs.usage('用法: $0 [选项] 文件') // usage string of applic
     type: 'boolean',
     default: true,
     description: '是否压缩文件'
+  }).option('babel', {
+    type: 'boolean',
+    default: true,
+    description: '是否使用 Babel 转译 JS 文件'
+  }).option('iife', {
+    type: 'boolean',
+    default: true,
+    description: 'JS 文件是否使用 IIFE（立即执行函数）包裹，启用压缩选项才可使用'
   }).option('obfuscate', {
     alias: 'o',
     type: 'boolean',
     default: false,
     description: '是否开启 JS 深度混淆'
+  }).option('sass', {
+    type: 'boolean',
+    default: true,
+    description: '是否使用 Sass 预处理器'
+  }).option('less', {
+    type: 'boolean',
+    default: true,
+    description: '是否使用 Less 预处理器'
+  }).option('raw', {
+    type: 'boolean',
+    default: false,
+    description: '是否上传原始文件'
   }).option('prefix', {
     alias: 'p',
     type: 'string',
@@ -67,10 +90,10 @@ const argv = yargs.usage('用法: $0 [选项] 文件') // usage string of applic
   }).option('base64', {
     type: 'boolean',
     default: false,
-    description: '是否处理成 base64 内容，而不上传 CDN'
+    description: '是否处理成 base64 内容，而不上传 OSS'
   }).option('dest', {
     type: 'string',
-    description: '本机文件系统路径，\n使用此参数将保存文件到指定路径，而不上传 CDN'
+    description: '本机文件系统路径，\n使用此参数将保存文件到指定路径，而不上传 OSS'
   }).option('output-simple', {
     type: 'boolean',
     default: false,
@@ -79,11 +102,6 @@ const argv = yargs.usage('用法: $0 [选项] 文件') // usage string of applic
     type: 'boolean',
     default: false,
     description: '是否合并文件'
-  }).option('refresh', {
-    alias: 'r',
-    type: 'boolean',
-    default: false,
-    description: '是否刷新 CDN 资源'
   }).option('config', {
     type: 'string',
     default: '',
@@ -93,10 +111,12 @@ const argv = yargs.usage('用法: $0 [选项] 文件') // usage string of applic
     default: false,
     description: '初始化配置文件'
   })
-  .help('help')
   .version('version', '显示版本信息', version)
   .alias('version', 'v')
-  .example('$0 filename.png', '上传资源到 CDN')
+  .example('$0 filename.png', '上传资源到 OSS')
+  .example('$0 refresh http://domain.com/filename.png', '刷新已存在的 OSS 资源缓存')
+  .help()
+  .wrap(95)
   .locale('zh_CN')
   .argv;
 
@@ -132,195 +152,182 @@ if (argv.config) {
   configFile = argv.config;
 }
 
-gulp.task('alioss', (cb) => {
+if (argv.raw) {
+  argv.compress = false;
+  argv.babel = false;
+  argv.iife = false;
+  argv.sass = false;
+  argv.less = false;
+}
+
+const aliossOptions = {
+  uriPrefix: argv.prefix || '',
+  uriName: argv.name || ''
+};
+
+// 上传资源到 OSS
+gulp.task('upload', () => {
 
   const isMulti = !!(argv.concat && typeof argv._ === 'object' && argv._.length > 1);
 
-  const aliossOptions = {
-    uriPrefix: argv.prefix || '',
-    uriName: argv.name || ''
+  const toBase64 = (file, enc, cb) => {
+    const Datauri = require('datauri');
+    const sizeOf = require('image-size');
+
+    if (file.isDirectory() || file.isStream()) {
+      return cb();
+    }
+
+    try {
+      const dimensions = sizeOf(file.path);
+
+      if (!argv.outputSimple && dimensions) {
+        log('width:', chalk.green(dimensions.width) + 'px', 'height:', chalk.green(dimensions.height) + 'px');
+      }
+    }
+    catch (e) {
+    }
+
+    const datauri = new Datauri(file.path);
+
+    file.contents = Buffer.from(datauri.content);
+
+    // console.log(datauri.content);
+
+    return cb(null, file);
   };
 
-  if (argv.refresh) {
-    oss.refresh(aliossOptions, (typeof argv._ === 'object' && argv._.length ? argv._[0] : argv._), argv.outputSimple).then(() => {
-      !argv.outputSimple && log(chalk.cyan('done.'));
-    }).then(cb);
+  const toClipboard = (file, enc, cb) => {
+    const copyPaste = require('copy-paste');
 
-    return;
-  }
-  else {
-    const toBase64 = (file, enc, cb) => {
-      const Datauri = require('datauri');
-      const sizeOf = require('image-size');
+    if (file.isDirectory() || file.isStream()) {
+      return cb();
+    }
 
-      if (file.isDirectory() || file.isStream()) {
-        return cb();
+    copyPaste.copy(file.contents, function() {
+      if (argv.outputSimple) {
+        console.log(chalk.green(file.path));
       }
-
-      try {
-        const dimensions = sizeOf(file.path);
-
-        if (!argv.outputSimple && dimensions) {
-          log('width:', chalk.green(dimensions.width) + 'px', 'height:', chalk.green(dimensions.height) + 'px');
-        }
+      else {
+        log('Copy to clipboard is OK:', chalk.green(file.path));
       }
-      catch (e) {
-      }
-
-      const datauri = new Datauri(file.path);
-
-      file.contents = Buffer.from(datauri.content);
-
-      // console.log(datauri.content);
 
       return cb(null, file);
-    };
+    });
+  };
 
-    const toClipboard = (file, enc, cb) => {
-      const copyPaste = require('copy-paste');
+  const run = () => {
+    return gulp.src(argv._)
+      .pipe($.if(/\.(js|css|less|scss|sass)$/i, $.sourcemaps.init()))
+      .pipe($.if(isMulti, $.concat(getConcatName())))
 
-      if (file.isDirectory() || file.isStream()) {
-        return cb();
-      }
+      .pipe($.if(/\.(htm|html)$/i, html(argv)))
+      .pipe($.if(file => argv.compress && /\.(htm|html)$/i.test(file.path), inlineCompress()))
+      .pipe($.if(file => argv.compress && /\.(htm|html)$/i.test(file.path), htmlmin({
+        collapseBooleanAttributes: true,
+        removeComments: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true
+      })).on('error', showError))
 
-      copyPaste.copy(file.contents, function() {
+      .pipe($.if(file => argv.compress && /\.(png|jpg|jpeg|gif|svg)$/i.test(file.path), imagemin([
+        imagemin.gifsicle({ interlaced: true, optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 7 })
+      ])).on('error', showError))
+
+      .pipe($.if(file => argv.babel && /\.js$/i.test(file.path), $.babel({
+        cwd: __dirname,
+        babelrc: false,
+        compact: false,
+        plugins: babelPlugins,
+        presets: [
+          [
+            require.resolve('@babel/preset-env', { paths: [__dirname] }), {
+              modules: false,
+              targets: { browsers }
+            }
+          ]
+        ]
+      })).on('error', showError))
+
+      .pipe($.if(file => argv.compress && argv.iife && /\.js$/i.test(file.path), wrapper({
+        header: '+function() {',
+        footer: '}()'
+      })).on('error', showError))
+
+      .pipe($.if('*.js', $.sourcemaps.write({ addComment: false })))
+
+      .pipe($.if(file => argv.compress && /\.js$/i.test(file.path), terser({
+        ie8: true,
+        safari10: true,
+        compress: {
+          drop_console: true
+        }
+      })).on('error', showError))
+      .pipe($.if('*.js', $.sourcemaps.write({ addComment: false })))
+
+      .pipe($.if(file => argv.obfuscate && /\.js$/i.test(file.path), javascriptObfuscator({ compact: true })))
+
+      .pipe($.if(file => argv.less && /\.less$/i.test(file.path), less()).on('error', showError))
+      .pipe($.if(file => argv.less && /\.less$/i.test(file.path), $.sourcemaps.write({ addComment: false })))
+
+      .pipe($.if(file => argv.sass && /\.(scss|sass)$/i.test(file.path), sass({ outputStyle: 'expanded' })).on('error', sass.logError))
+      .pipe($.if(file => argv.sass && /\.(scss|sass)$/i.test(file.path), $.sourcemaps.write({ addComment: false })))
+
+      .pipe($.if(file => argv.compress && /\.(css|less|scss|sass)$/i.test(file.path), $.postcss([ use({
+        modules: [ 'postcss-pxtorem' ],
+        options: {
+          'postcss-pxtorem': pxtoremDefault
+        }
+      }), cssnano({
+        preset: ['default', {
+          autoprefixer: false,
+          zindex: false,
+          reduceIdents: false,
+          reduceTransforms: false
+        }]
+      }), autoprefixer({
+        overrideBrowserslist: browsers
+      }) ])))
+      .pipe($.if(file => argv.compress && /\.(css|less|scss|sass)$/i.test(file.path), $.sourcemaps.write({ addComment: false })))
+
+  };
+
+  if (argv.dest) {
+    return run()
+      .pipe($.if(!!argv.base64, through2.obj({ maxConcurrency: 8 }, toBase64)))
+      .pipe($.if(!!argv.name, $.rename(argv.name)))
+      .pipe(gulp.dest(argv.dest)).on('data', (file) => {
         if (argv.outputSimple) {
           console.log(chalk.green(file.path));
         }
         else {
-          log('Copy to clipboard is OK:', chalk.green(file.path));
+          log('OK: ' + chalk.green(file.path));
         }
-
-        return cb(null, file);
+      }).on('end', () => {
+        !argv.outputSimple && log(chalk.cyan('done.'));
       });
-    };
-
-    if (!argv.compress) {
-      const run = () => {
-        return gulp.src(argv._)
-          .pipe($.if(isMulti, $.concat(getConcatName())))
-          .pipe($.if(/\.(png|jpg|jpeg|gif|svg)$/i, imagemin([
-            imagemin.gifsicle({ interlaced: true, optimizationLevel: 3 }),
-            imagemin.jpegtran({ progressive: true }),
-            imagemin.optipng({ optimizationLevel: 7 })
-          ])).on('error', showError))
-      };
-
-      if (argv.dest) {
-        return run().pipe($.if(!!argv.name, $.rename(argv.name))).pipe(gulp.dest(argv.dest)).on('data', (file) => {
-          if (argv.outputSimple) {
-            console.log(chalk.green(file.path));
-          }
-          else {
-            log('OK: ' + chalk.green(file.path));
-          }
-        }).on('end', () => {
-          !argv.outputSimple && log(chalk.cyan('done.'));
-        });
-      }
-      else {
-        return run().pipe(oss.upload(aliossOptions, argv.outputSimple)).on('end', () => {
-          !argv.outputSimple && log(chalk.cyan('done.'));
-        });
-      }
-    }
-    else {
-      const run = () => {
-        return gulp.src(argv._)
-          .pipe($.if(/\.(js|css|less|scss|sass)$/i, $.sourcemaps.init()))
-          .pipe($.if(isMulti, $.concat(getConcatName())))
-
-          .pipe($.if(/\.(htm|html)$/i, html()))
-          .pipe($.if(/\.(htm|html)$/i, inlineCompress()))
-          .pipe($.if(/\.(htm|html)$/i, htmlmin({
-            collapseBooleanAttributes: true,
-            removeComments: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            removeRedundantAttributes: true,
-            useShortDoctype: true,
-            removeEmptyAttributes: true
-          })).on('error', showError))
-
-          .pipe($.if(/\.(png|jpg|jpeg|gif|svg)$/i, imagemin([
-            imagemin.gifsicle({ interlaced: true, optimizationLevel: 3 }),
-            imagemin.jpegtran({ progressive: true }),
-            imagemin.optipng({ optimizationLevel: 7 })
-          ])).on('error', showError))
-
-          .pipe($.if('*.js', $.babel({
-            cwd: __dirname,
-            babelrc: false,
-            compact: false,
-            plugins: babelPlugins,
-            presets: [
-              [
-                require.resolve('@babel/preset-env', { paths: [__dirname] }), {
-                  modules: false,
-                  targets: { browsers }
-                }
-              ]
-            ]
-          })).on('error', showError))
-          .pipe($.if('*.js', $.sourcemaps.write({ addComment: false })))
-          .pipe($.if('*.js', $.uglify({
-            ie8: true,
-            compress: {
-              drop_console: true,
-              drop_debugger: true
-            }
-          })).on('error', showError))
-          .pipe($.if('*.js', $.sourcemaps.write({ addComment: false })))
-          .pipe($.if((file) => /\.js$/i.test(file.path) && argv.obfuscate, javascriptObfuscator({ compact: true })))
-
-          .pipe($.if(/\.less$/i, less()).on('error', showError))
-          .pipe($.if(/\.less$/i, $.sourcemaps.write({ addComment: false })))
-          .pipe($.if(/\.(scss|sass)$/i, sass({ outputStyle: 'expanded' })).on('error', sass.logError))
-          .pipe($.if(/\.(scss|sass)$/i, $.sourcemaps.write({ addComment: false })))
-          .pipe($.if(/\.(css|less|scss|sass)$/i, $.postcss([ use({
-            modules: [ 'postcss-pxtorem' ],
-            options: {
-              'postcss-pxtorem': pxtoremDefault
-            }
-          }), cssnano({
-            preset: ['default', {
-              autoprefixer: false,
-              zindex: false,
-              reduceIdents: false,
-              reduceTransforms: false
-            }]
-          }), autoprefixer({
-            overrideBrowserslist: browsers
-          }) ])))
-          .pipe($.if(/\.(css|less|scss|sass)$/i, $.sourcemaps.write({ addComment: false })))
-      };
-
-      if (argv.dest) {
-        return run()
-          .pipe($.if(!!argv.base64, through2.obj({ maxConcurrency: 8 }, toBase64)))
-          .pipe($.if(!!argv.name, $.rename(argv.name)))
-          .pipe(gulp.dest(argv.dest)).on('data', (file) => {
-            if (argv.outputSimple) {
-              console.log(chalk.green(file.path));
-            }
-            else {
-              log('OK: ' + chalk.green(file.path));
-            }
-          }).on('end', () => {
-            !argv.outputSimple && log(chalk.cyan('done.'));
-          });
-      }
-      else {
-        return run()
-          .pipe($.if(!!argv.base64, through2.obj({ maxConcurrency: 8 }, toBase64)))
-          .pipe($.if(!!argv.base64, through2.obj({ maxConcurrency: 8 }, toClipboard)))
-          .pipe($.if(!argv.base64, oss.upload(aliossOptions, argv.outputSimple))).on('end', () => {
-            !argv.outputSimple && log(chalk.cyan('done.'));
-          });
-      }
-    }
+  }
+  else {
+    return run()
+      .pipe($.if(!!argv.base64, through2.obj({ maxConcurrency: 8 }, toBase64)))
+      .pipe($.if(!!argv.base64, through2.obj({ maxConcurrency: 8 }, toClipboard)))
+      .pipe($.if(!argv.base64, oss.upload(aliossOptions, argv.outputSimple))).on('end', () => {
+        !argv.outputSimple && log(chalk.cyan('done.'));
+      });
   }
 
+});
+
+// 刷新 OSS 资源
+gulp.task('refresh', (cb) => {
+  oss.refresh(aliossOptions, argv.url, argv.outputSimple).then(() => {
+    !argv.outputSimple && log(chalk.cyan('done.'));
+  }).then(cb);
 });
 
 if (argv.initConfig) {
@@ -422,9 +429,19 @@ else {
   if (argv._.length) {
     !argv.outputSimple && log(chalk.cyan('starting...'));
 
-    gulp.start('alioss');
+    if (argv._[0] === 'refresh') {
+      if (argv.url) {
+        gulp.start('refresh');
+      }
+      else {
+        yargs.showHelp('log');
+      }
+    }
+    else {
+      gulp.start('upload');
+    }
   }
   else {
-    yargs.showHelp();
+    yargs.showHelp('log');
   }
 }
